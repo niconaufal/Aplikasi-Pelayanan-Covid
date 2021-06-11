c02adbbe094f226061bddf739313259614eeab28
nico_naufal <76398519+niconaufal21@users.noreply.github.com>
2021-06-11 22:22:21 +0700

    Update registrasi.php

--- halaman/registrasi.php
+++ halaman/registrasi.php
@@ -9,12 +9,10 @@
         <a href="https://wa.me/+6285717917864" target="_blank" class="btn btn-success"><span class="iconify" data-icon="akar-icons:whatsapp-fill" data-inline="false"></span> Whatshapp</a>
         </div>
     </div>
-    </div>
     <div class="col-lg-6 mb-4">
     <div class="symptom d-flex">
         <div class="img">
         <img src="images/symptom_cough.png" alt="Image" class="img-fluid">
-        </div>
         <div class="text">
         <h3> User (Covid Ranger)</h3>
         <p>Daftarkan diri sebagai covid ranger di halaman registrasi</p>
