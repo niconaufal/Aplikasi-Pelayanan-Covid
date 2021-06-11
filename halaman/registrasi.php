f7cc4301eaaf400407924fc2dc2579e4f16eaaf8
nico_naufal <76398519+niconaufal21@users.noreply.github.com>
2021-06-11 22:32:36 +0700

    Update registrasi.php

--- halaman/registrasi.php
+++ halaman/registrasi.php
@@ -1,15 +1,15 @@
 <div class="row">
-    <div class="col-lg-6 mb-4">
-    <div class="symptom d-flex">
-        <div class="img">
-        <img src="images/symptom_high-fever.png" alt="Image" class="img-fluid">
-        </div>
-        <div class="text">
-        <h3>Admin</h3>
-        <p>Daftarkan Diri Melalui Admin</p>
-        <a href="https://wa.me/+6285717917864" target="_blank" class="btn btn-success"><span class="iconify" data-icon="akar-icons:whatsapp-fill" data-inline="false"></span> Whatshapp</a>
-        </div>
-      </div>
+      <div class="col-lg-6 mb-4">
+      <div class="symptom d-flex">
+          <div class="img">
+          <img src="images/symptom_high-fever.png" alt="Image" class="img-fluid">
+           </div>
+           <div class="text">
+           <h3> Admin </h3>
+           <p> Daftarkan Diri Melalui Admin </p>
+           <a href="https://wa.me/+6285717917864" target="_blank" class="btn btn-success"><span class="iconify" data-icon="akar-icons:whatsapp-fill" data-inline="false"></span> Whatshapp</a>
+            </div>
+          </div>
         </div>
         
     <div class="col-lg-6 mb-4">
