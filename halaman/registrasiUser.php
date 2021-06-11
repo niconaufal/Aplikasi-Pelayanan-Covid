0599e39d2903e97753095709ed6929e4d5de9a72
nico_naufal <76398519+niconaufal21@users.noreply.github.com>
2021-06-11 22:23:48 +0700

    Update registrasiUser.php

--- halaman/registrasiUser.php
+++ halaman/registrasiUser.php
@@ -1,9 +1,10 @@
 <form action="" method="post">
     <label for=""><span class="iconify" data-icon="fluent:number-symbol-16-regular" data-inline="false"></span> Masukkan NIK Anda</label>
     <input type="number" name="nik" class="form-control" placeholder="Nik Anda Akan Dicek..." onkeyup="showHint(this.value)">
-    <span id="searchNik"></span></form>
+    <span id="searchNik"></span>
 
 <script>
+    </form>
 function showHint(str) {
   if (str.length == 0) {
     document.getElementById("searchNik").innerHTML = "";
